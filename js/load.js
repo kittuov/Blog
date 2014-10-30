ccecb5c3fe8cd9296e908a89a37fe3de84d4f4ed
kittuov <krishnachaitanya.kornepati@gmail.com>
2014-10-30 18:04:59 +0530

    DONE IT

--- js/load.js
+++ js/load.js
@@ -1,16 +1,31 @@
 var i=0;
-var main_section;
+//var main_section;
+var a=0;
 
 function init()
 {
-    main_section=document.getElementById("main_section").innerHTML;
-    a=$("#main_section").children().length;
-    addcls(i);    
+   // main_section=document.getElementById("main_section").innerHTML;
+    //a=$("#main_section").children().length;
+    $(".blog_post").addClass("blog_load");
+    $(".blog_post").click(doit);
 }
-function addcls ( k){
-    if (k<=a) 
-    {b=(k+1).toString();
-    $(".blog_post:nth-child("+b+")").addClass("blog_load");i++; setTimeout(addcls(k+1),200);}
-    else
-    {clearInterval();}
+function doit(e)
+{
+    //$(".blog_post").removeClass("post_load");
+    $(".blog_post").removeClass("blog_load");
+    //$(".blog_post").hide();
+    //$(this).addClass("post_load");
+    //$(this).show();
+    $(this).animate({opacity:'1',transform:"scaleX(1.5)"},1000,"swing",hide_blog);
+ //   $(this).css("height","auto");
+ //   $(this).children("article").css("overflowY","scroll");
+    
+}
+function hide_blog()
+{
+    $(".blog_post").hide();
+    $(this).show();
+    $(this).css("height","auto");
+    $(this).children("article").css("overflowY","scroll");
+    $(this).children("article").css("padding-right","10px");
 }
